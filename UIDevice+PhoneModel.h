e1914d406cad617ceb24cb5fa010c32e21d96fc5
袁斌 <bin.yuan@baidao.com>
2017-03-03 15:41:55 +0800

    Add Mobile device models

---
+++ UIDevice+PhoneModel.h
@@ -0,0 +1,15 @@
+//
+//  UIDevice+PhoneModel.h
+//  TestDemo
+//
+//  Created by DefaultYuan on 2017/3/3.
+//  Copyright © 2017年 https://github.com/DefaultYuan/UIDevice-PhoneModel.git. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIDevice (PhoneModel)
+
+- (nonnull NSString *) deviceModel;
+
+@end
